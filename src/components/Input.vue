43f5491c96fa9efea08c8e5a92fc000fe554a69c
nikzz-tea <nikita.red638@gmail.com>
2024-03-06 13:13:24 +0300

    create input component

---
+++ src/components/Input.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  placeholder?: string;
+}>();
+</script>
+
+<template>
+  <input
+    class="rounded-md border-4 border-zinc-600 bg-transparent px-3 py-1 text-xl text-white"
+    type="text"
+    :placeholder="placeholder"
+  />
+</template>

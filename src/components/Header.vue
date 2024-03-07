2fbf953957f9ca5f67f7f4254a327d44fd5ab154
nikzz-tea <nikita.red638@gmail.com>
2024-03-07 04:56:49 +0300

    feat: display all task's info in task card

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,5 @@
 <template>
-  <header class="text-center text-5xl text-white">Task Manager</header>
+  <header class="text-center text-5xl font-bold text-white">
+    Task Manager
+  </header>
 </template>

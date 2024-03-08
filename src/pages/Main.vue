75df26bcbf544b487c8987f0ed9928f5806cfbac
nikzz-tea <nikita.red638@gmail.com>
2024-03-08 03:15:08 +0300

    feat: add button to go to start screen

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -7,5 +7,10 @@ import TaskList from "../components/Tasks/TaskList.vue";
   <div class="mx-auto flex max-w-[700px] flex-col gap-8 pt-8">
     <CreateForm />
     <TaskList />
+    <router-link class="text-center" to="/">
+      <span class="text-white duration-150 hover:text-zinc-300"
+        >На стартовую</span
+      >
+    </router-link>
   </div>
 </template>
